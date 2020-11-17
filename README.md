3412a0904ec03480e4e6984eae0ef82b10c550e7
JakesProjects1 <jacob@kagon.net>
2020-11-17 15:19:27 -0500

    done

--- README.md
+++ README.md
@@ -5,17 +5,17 @@
 
 ## Deliverables
 
-- A function that selects the title and changes the text
-- A function that selects the title and changes styling
-- A function that adds an item to the "favorite things" list
-- A function that selects and console.logs each list item
-- A function that loops through each list item and changes its text and style
+<!-- - A function that selects the title and changes the text
+- A function that selects the title and changes styling -->
+<!-- - A function that adds an item to the "favorite things" list -->
+<!-- - A function that selects and console.logs each list item -->
+<!-- - A function that loops through each list item and changes its text and style -->
 
 ## Strech Deliverable
 
 - Write a function that creates a button, that when clicked, will change the background color. (click event)
 - Make a branch and "pull request" for your code.
-
+ 
 ## Example
 
 https://share.getcloudapp.com/d5upe6pJ
--- script.js
+++ script.js
@@ -1,1 +1,65 @@
 // Not linked
+function changeTitleText() {
+    header = document.getElementsByTagName('h1');
+    header[0].innerText = "My Favs";
+}
+
+changeTitleText()
+
+function changeColor() {
+    style = document.getElementsByTagName('h1');
+    style[0].style.color = 'red';
+}
+
+changeColor()
+
+function addListItem() {
+   ul = document.getElementById('items');
+   li = document.createElement('li');
+   li.className = "list-group-item";
+   li.innerText = "Bread";
+   ul.appendChild(li);
+}
+
+addListItem()
+
+function outputList() {
+   items = document.getElementById('items').querySelectorAll('li');
+   items.forEach(item => console.log(item.innerText))
+}
+
+outputList()
+
+function changeStyle() {
+    items = document.getElementById('items').querySelectorAll('li');
+    items.forEach(item => {
+       item.innerText = "Bread";
+       item.style.color = "Blue"})
+ }
+
+ changeStyle()
+
+ function addButton(){
+     let button = document.createElement('button');
+     button.innerText = "Button";
+     button.addEventListener('click', function(event){
+        //  alert("You've clicked this");
+        const btnColor = ["btn btn-primary", "btn btn-secondary", "btn btn-success", "btn btn-warning", "btn btn-info", "btn btn-light", "btn btn-dark", "btn btn-danger"]
+        button.className = btnColor[Math.floor(Math.random() * btnColor.length)]
+    //  button.style.backgroundColor = getRandomColor()
+
+     })
+     let div = document.getElementsByClassName('container');
+     div[1].append(button);
+ }
+
+   addButton()
+
+
+   
+
+
+
+
+
+
